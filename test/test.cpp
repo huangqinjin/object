735451b7c8af571f34c6f1bfd2d8ccddee39dc07
huangqinjin <huangqinjin@gmail.com>
2019-03-09 14:34:41 +0800

    test cast and ownership

--- test/test.cpp
+++ test/test.cpp
@@ -88,3 +88,55 @@ TEST_CASE("constructor and destructor")
         CHECK(tracker::count == 1);
     }
 }
+
+TEST_CASE("object cast")
+{
+    object o1;
+    object o2(2);
+
+    SECTION("cast empty object")
+    {
+        CHECK_THROWS_AS(object_cast<int>(o1), bad_object_cast);
+        CHECK_NOTHROW(object_cast<int>(&o1));
+        CHECK(object_cast<int>(&o1) == nullptr);
+    }
+
+    SECTION("cast non-empty object")
+    {
+        CHECK(o2.type() == typeid(int));
+
+        CHECK_NOTHROW(object_cast<int>(o2));
+        CHECK_THROWS_AS(object_cast<float>(o2), bad_object_cast);
+
+        CHECK_NOTHROW(object_cast<int>(&o2));
+        CHECK_NOTHROW(object_cast<float>(&o2));
+
+        CHECK(object_cast<int>(o2) == 2);
+        CHECK(*object_cast<int>(&o2) == 2);
+        CHECK(object_cast<float>(&o2) == nullptr);
+        CHECK(std::addressof(object_cast<int>(o2)) == object_cast<int>(&o2));
+
+        CHECK(unsafe_object_cast<int>(o2) == 2);
+        CHECK(*unsafe_object_cast<int>(&o2) == 2);
+        CHECK(std::addressof(unsafe_object_cast<int>(o2)) == unsafe_object_cast<int>(&o2));
+        CHECK(object_cast<int>(&o2) == unsafe_object_cast<int>(&o2));
+    }
+}
+
+TEST_CASE("shared ownership")
+{
+    object o1(1);
+    object o2(o1);
+
+    SECTION("take address")
+    {
+        CHECK(object_cast<int>(&o1) == object_cast<int>(&o2));
+    }
+
+    SECTION("update object")
+    {
+        ++object_cast<int>(o1);
+        CHECK(object_cast<int>(o1) == 2);
+        CHECK(object_cast<int>(o2) == 2);
+    }
+}
