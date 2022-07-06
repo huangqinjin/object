f6fe403648cb69021c9cc6fcd880294074399cdd
huangqinjin <huangqinjin@gmail.com>
2022-07-06 19:47:55 +0800

    copyright 2022

--- object.hpp
+++ object.hpp
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2019-2022 Huang Qinjin (huangqinjin@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
 #ifndef OBJECT_HPP
 #define OBJECT_HPP
 
